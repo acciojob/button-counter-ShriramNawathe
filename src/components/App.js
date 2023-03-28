3d4ee462976451ecaa3ee9e719089a8ef51818a8
Shriram Rajeshwar Nawathe <108632713+ShriramNawathe@users.noreply.github.com>
2023-03-28 17:50:46 +0530

    solve 2.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,23 @@
-
-
-
-import React from "react";
-import './../styles/App.css';
-import React, { useState } from 'react';
-
-function ButtonClickCounter() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <button onClick={handleClick}>Click me</button>
-      <p>Button clicked {count} times</p>
+      <p>
+        <h1>Button clicked {count} times
+          </h1></p>
+      <br />
+      <button onClick={handleClick}>Click me</button>
+      
     </div>
-  )
-}
-}
-export default App
+  );
+};
+
+export default App;
